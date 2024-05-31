f561117af3562a089d26f785df5c9cdb68196cd6
link-05 <139077052+link-05@users.noreply.github.com>
2024-05-31 15:43:18 +0000

    5/29/24 fixed the design of the frq solutions, 5/30/24 created another page to be linked at the main page 5/31/24, fix the button to be aligned at the center.

--- script.js
+++ script.js
@@ -18,21 +18,24 @@
 //   });
 //Figure out later does not work.
 
+//Pulls link of each file folder in the repo folder. The url will be taken and placed into a mini box.
 function getFolderLinks() {
   const url = "https://github.com/link-05/APCSA_MP3/tree/main/frq-solutions";
   const apiUrl = `https://api.github.com/repos/link-05/APCSA_MP3/contents/frq-solutions`;
   fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      data.forEach(item => {
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((item) => {
         if (item.type === "dir") {
           createMiniBox(item.name, url + "/" + item.name);
+          //The slash will add the item name to a universal link and end up creating a url that is individualized for each folder.
         }
       });
     })
-    .catch(error => console.error("Error fetching folders", error));
+    .catch((error) => console.error("Error fetching folders", error));
 }
 
+//Function that will take the link, name of folder
 function createMiniBox(title, url) {
   const miniBox = document.createElement("article");
   miniBox.className = "mini-box";
@@ -44,8 +47,35 @@ function createMiniBox(title, url) {
   link.target = "_blank"; // Add this attribute will force the window to open
 
   miniBox.appendChild(link);
-  
+
   document.body.appendChild(miniBox);
 }
 
-getFolderLinks();
+if (window.location.pathname === "/hub.html") {
+  // Execute getFolderLinks() only on hub.html
+  getFolderLinks();
+}
+
+//Code to make the button move left and right
+// Wrap the code in a DOMContentLoaded event listener
+if (window.location.pathname === "/index.html") {
+  document.addEventListener("DOMContentLoaded", function () {
+    const button = document.querySelector("button");
+    let buttonLeft = 0;
+    let buttonDirection = 1; // 1 for right, -1 for left
+    function moveButton() {
+      buttonLeft += buttonDirection;
+      button.style.left = buttonLeft + "px";
+      if (
+        buttonLeft >= window.innerWidth - button.offsetWidth ||
+        buttonLeft <= 0
+      ) {
+        buttonDirection *= -1;
+      }
+      requestAnimationFrame(moveButton);
+    }
+    moveButton();
+  });
+}
+
+//end of the mini window that hold the links to the different folders
